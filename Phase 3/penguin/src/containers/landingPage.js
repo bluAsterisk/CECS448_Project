d0afc5fa38530d8fd47f4aa47e147dadeef3f3d8
Evan Pascual <evanpascual@evans-imac.home>
2020-11-12 11:47:03 -0800

    delete unecessary files + initialize landing page

---
+++ Phase 3/penguin/src/containers/landingPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function landingPage(){
+    return(
+        <div>
+            test :)
+        </div>
+    );
+}
+export default landingPage
---
+++ Phase 3/penguin/src/styles/lightMode.js
@@ -0,0 +1,1 @@
+const converted = { ".lightMode": { textAlign: "center" } }
