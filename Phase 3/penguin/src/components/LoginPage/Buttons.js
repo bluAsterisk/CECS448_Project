aa536b5309290d5df2a4fde140b128c0a137198e
bluAsterisk <k3vinphan@hotmail.com>
2020-11-23 23:18:32 -0800

    Added buttons for Facebook & Google
    Still figuring out spacing

--- Phase 3/penguin/src/components/LoginPage/Buttons.js
+++ Phase 3/penguin/src/components/LoginPage/Buttons.js
@@ -1,22 +1,48 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid';
+
 
 function Buttons(){
     return(
         <div>
-            <Button 
-                variant='contained' 
-                size = "large"
-                id="login" 
-                color="secondary"
-                to='/login'>Login
-            </Button>
-            <Button
-                variant= 'contained'
-                size = "large" 
-                id="signup" 
-                color = "primary">Sign Up
-            </Button>
+            <Grid container>
+                <Grid item>
+                    <Button 
+                        variant='contained' 
+                        size = "large"
+                        id="login" 
+                        color="secondary"
+                        to='/login'>Login
+                    </Button>
+                    <Button
+                        variant= 'contained'
+                        size = "large" 
+                        id="signup" 
+                        color = "primary">Sign Up
+                    </Button>
+                </Grid>
+            </Grid>
+            <Grid container>
+                <Grid item>
+                    <Button
+                        variant= 'contained'
+                        size = "large" 
+                        id="signup" 
+                        color = "primary">Login with Facebook
+                    </Button>
+                </Grid>
+            </Grid>
+            <Grid container>
+                <Grid item>
+                    <Button
+                        variant= 'contained'
+                        size = "large" 
+                        id="signup" 
+                        color = "primary">Login with Google
+                    </Button>
+                </Grid>
+            </Grid>
         </div>
     );
 }
