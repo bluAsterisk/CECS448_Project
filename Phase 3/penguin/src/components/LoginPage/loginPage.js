0d02d2644a0544492b379a37bc9946275438dd03
bluAsterisk <k3vinphan@hotmail.com>
2020-11-23 19:34:09 -0800

    Spacing bug fixed when mouseover

--- Phase 3/penguin/src/components/LoginPage/loginPage.js
+++ Phase 3/penguin/src/components/LoginPage/loginPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import TextFieldPassword from './TextFieldPassword'
 import Buttons from './Buttons'
 import TextField from '@material-ui/core/TextField';
+import TextFieldPassword from './TextFieldPassword'
 import Grid from '@material-ui/core/Grid';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 
@@ -9,7 +9,7 @@ import AccountCircle from '@material-ui/icons/AccountCircle';
 function LoginPage(){
     return(
         <div>
-            <Grid container spacing={1} justify ='center' alignItems="center">
+            <Grid container spacing={0} justify ='center' alignItems="center">
                 <Grid item>
                     <TextField id="input-with-icon-grid" label="Username" />
                     <AccountCircle />
